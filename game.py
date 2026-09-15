b0c507b6a5ab9696d98f576c8af87f9a9ab07ed2
McKFerris <ferris.mck@gmail.com>
2026-09-15 15:14:12 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,8 +3,17 @@ from hero import Hero
 
 
 ARENA_NAME = "The Battle Circle"
-
-
+def battle(hero:Hero, enemy:Goblin):
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage=hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage=enemy.attack()
+            hero.take_damage(enemy_damage)
+    if hero.is_alive:
+        print(f"{hero.name} won the battle")
+    else:
+        print(f"{enemy.name} won the battle")
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
@@ -12,15 +21,18 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Flour")
-    goblin2=Goblin("Buford")
+    goblin2 =Goblin("Buford")
+    hero = Hero("Briar")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    
-    briar = Hero("Briar")
-    briarAttacknumber=briar.attack()
-    goblin.take_damage(briarAttacknumber)
-    briar.battlecry()
+    print(f"{hero.name} enters the arena with {hero.health} health.")
 
+    heroAttacknumber=hero.attack()
+    goblin.take_damage(heroAttacknumber)
+    hero.battlecry()
+
+    battle(hero, goblin2)
 
 if __name__ == "__main__":
     main()
+
